e588ab1459d5b7dc1034a021b3292de107a94313
rodrigo gomes <rodgomesc@gmail.com>
2022-04-29 11:05:20 -0300

    chore: finish appClip configuration

--- ios/rnPOcAppClip/ViewController.m
+++ ios/rnPOcAppClip/ViewController.m
@@ -1,11 +1,11 @@
 //
 //  ViewController.m
-//  rnPOcAppClip
-//
-//  Created by rodrigo gomes on 29/04/22.
+//  ReactNativeTestClip
 //
 
 #import "ViewController.h"
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
 
 @interface ViewController ()
 
@@ -13,10 +13,32 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+- (void)loadView {
+  #if DEBUG
+  // When debugging we load the js bundle from the Metro Bundler
+  // running on your development machine. "index" is the name of the
+  // js file used as an entry point (don't include the extension).
+  NSURL *jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+  #else
+  // For release builds we add the js bundle to the build.
+  // By default the js bundler will create a file called
+  // "main.jsbundle" for us.
+  NSURL *jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  #endif
+
+  RCTRootView *rootView = [[RCTRootView alloc] 
+  // moduleName corresponds to the appName used for the
+  // app entry point in "index.js"
+initWithBundleURL:jsCodeLocation moduleName:@"rnPOcAppClip" 
+initialProperties:nil launchOptions:nil];
+  // Default to a white background.
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f 
+green:1.0f blue:1.0f alpha:1];
+  self.view = rootView;
 }
 
-
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view.
+}
 @end
